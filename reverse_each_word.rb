40af0634dd10d9583d169ee31b2fd2273e6c3da7
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-11 03:37:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 require "pry"
 def reverse_each_word (phrase)
   binding.pry 
+  array= phrase.split
   phrases = ("Hello there, and how are you?")
     phrase.each do |phrases|
     phrases << "Hello there, and how are you?"
